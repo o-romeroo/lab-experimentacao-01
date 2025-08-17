a1aacfc6f61fbe10397dae77c4d786f4a9d8425e
Lucas Randazzo <lucasrandazzo2@gmail.com>
2025-08-17 20:49:43 -0300

    Refactor median calculation functions and add main execution logic to process repository data

--- data_summarization.py
+++ data_summarization.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
-df = pd.read_csv("repository_csv")
-
 def median_repository_age(df):
     """Calcula a mediana da idade dos repositórios.
 
@@ -22,7 +20,7 @@ def median_merged_pull_requests_count(df):
     Retorna:
     - float: mediana do número de pull requests mergeados com 2 casas decimais.
     """
-    return round(df["merged_prs_count"].median(), 2)
+    return round(df["merged_pull_requests_count"].median(), 2)
 
 def median_releases_count(df):
     """Calcula a mediana do número de releases.
@@ -55,7 +53,9 @@ def median_primary_language(df):
     Retorna:
     - float: mediana do número de linguagens principais com 2 casas decimais.
     """
-    return round(df["primary_language"].value_counts().median(), 2)
+    freq = df["primary_language"].value_counts().sort_index()
+    median_idx = int((len(freq) - 1) / 2)
+    return freq.index[median_idx]
 
 def median_closed_issues_percentage(df):
     """Calcula a mediana da porcentagem de issues fechadas.
@@ -67,3 +67,20 @@ def median_closed_issues_percentage(df):
     - float: mediana da porcentagem de issues fechadas com 2 casas decimais.
     """
     return round(df["closed_issues_percentage"].median(), 2)
+
+def main():
+    df = pd.read_csv("repository_data.csv")
+
+    median_data = {
+        "median_repository_age": median_repository_age(df),
+        "median_merged_pull_requests": median_merged_pull_requests_count(df),
+        "median_releases": median_releases_count(df),
+        "median_hours_since_last_update": median_hours_since_last_update(df),
+        "median_primary_language": median_primary_language(df),
+        "median_closed_issues_percentage": median_closed_issues_percentage(df)
+    }
+    median_df = pd.DataFrame([median_data])
+    median_df.to_csv("median_repository_data.csv", index=False)
+
+if __name__ == "__main__":
+    main()
